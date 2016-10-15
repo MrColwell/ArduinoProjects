0ff482567d50a79f9a9847f2dc659baa23f5167f
MrColwell <MrColwell@users.noreply.github.com>
2016-10-15 12:01:56 -0300

    Create DataLoggerMain.py

---
+++ DataLoggerMain.py
@@ -0,0 +1,11 @@
+# readDataLoggers
+# A.Colwell (2016)
+'''A test run of a set of data loggers placed in a small watershed important to the growth of Atlantic Canada Salmon.
+This program helps to interpret the data.
+To do:
+- read data (Time, TempInside, TempOutside, ?)
+- report data start and end dates
+- provide graphical representation of data collected
+- obtain relative weather for time of data from Environment Canada (YSJ)
+- ??
+'''
